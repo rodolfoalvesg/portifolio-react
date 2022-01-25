e732f83c8c482a1c5743c41c2945b4310f27f35f
Rodolfo Alves <rodolfoalves.inf@gmail.com>
2022-01-25 17:21:00 -0300

    added icon and option whatsapp send

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,6 +4,7 @@ import facebook from '../../assets/facebook.svg'
 import instagram from '../../assets/instagram.svg'
 import linkedin from '../../assets/linkedin.svg'
 import logo from '../../assets/logo.svg'
+import whatsapp from '../../assets/whatsapp.svg'
 
 function Header() {
   return (
@@ -11,9 +12,11 @@ function Header() {
       <img src={logo} alt="logo" id="logo" />
       <div>
           <ul>
+          <li><a href="https://api.whatsapp.com/send?1=pt_BR&phone=83933008375&text=Olá" target="blank"><img src={whatsapp} alt="whatsapp"/></a></li>
             <li><a href="https://www.facebook.com/rodolfoalvesg" target="blank"><img src={facebook} alt="facebook"/></a></li>
             <li><a href="https://www.instagram.com/rodolfo.alvesg" target="blank"><img src={instagram} alt="instagram"/></a></li>  
             <li><a href="https://www.linkedin.com/in/rodolfoalvesg" target="blank"><img src={linkedin} alt="linkedin"/></a></li>
+            
           </ul>   
 
           <a href="https://drive.google.com/file/d/1thPWrghiZkL_hMUwk2vIQbqKJT43js9T/view?usp=sharing" target="blank" className="btn-cv">DOWNLOAD CV</a>
